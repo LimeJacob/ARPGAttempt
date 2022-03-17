495c631d2578d42757840e1e7f57c34817f1e7a4
LimeJacob <89800548+LimeJacob@users.noreply.github.com>
2022-03-17 13:07:38 -0400

    go

---
+++ Just Do It/Assets/Stat.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System;
+
+[System.Serializable]
+public class Stat
+{
+    [SerializeField] private float baseValue;
+    [SerializeField] public float baseMultiplier { get; set; }
+
+    public float GetValue() 
+    {
+        return baseValue * baseMultiplier;
+    }
+}
